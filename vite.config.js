158a99a1a02070a1a862e0c8b34b1f307f3bc447
code-with-zunaid <zunaidalam707070@gmail.com>
2025-05-02 11:44:14 +0530

    add git page

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  base: '/Spin/', // 👈 This is required for GitHub Pages deployment
+  plugins: [react(), tailwindcss()],
 })
-
-
